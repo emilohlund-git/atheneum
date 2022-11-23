e6fe01f47a9bbd043c0482eb959730b0137620da
Emil Ölund <emil.olund@jayway.com>
2022-11-23 18:44:58 +0100

    chore: :technologist:

--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,15 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Button } from '@emilohlund-git/tailwind-component-lib';
-import '@emilohlund-git/tailwind-component-lib/dist/tailwind.css';
+import { Button } from '../src';
+import '@atheneum/atheneum/dist/tailwind.css';
 
 const App = () => {
   return (
     <div>
-      <Button variant="primary" size="medium" text="Button" />
+      <Button variant="primary-outline" size="medium">
+        TEXT
+      </Button>
     </div>
   );
 };
